391f0e3f54fcd57450ff38d5cd80c06c30e5be52
Snoupax <snowpy1544@gmail.com>
2022-06-16 00:26:06 +0200

    feat: adding const gameInProgress in Game and drilling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,22 +1,86 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Board from "./Board";
 
 export default function Game() {
-  const[playerActive, setPlayerActive] = useState('X')
-  const changePlayer = () =>{
-    let test = 0;
-    if(playerActive == 'X'){
-        console.log(test)
-        setPlayerActive('O')
-    }else {
-        setPlayerActive('X')
-        console.log(test)
+  const [gameInProgress, setGameInProgress] = useState({
+    gameStatus: 0,
+    players: {
+      X: {
+        win: 0,
+      },
+      O: {
+        win: 0,
+      },
+    },
+    playerActive: "X",
+    game: {
+      n1: "",
+      n2: "",
+      n3: "",
+      n4: "",
+      n5: "",
+      n6: "",
+      n7: "",
+      n8: "",
+      n9: "",
+    },
+  });
 
+  const winOrNot = (game, gameStatus) => {
+    console.log(game.n1)
+    console.log(game.n2)
+    if (
+      (game.n1 === game.n2 && game.n2 === game.n3 && game.n1 !== "") ||
+      (game.n4 === game.n5 && game.n5 === game.n6 && game.n1 !== "") ||
+      (game.n7 === game.n8 && game.n8 === game.n9 && game.n1 !== "")
+    ) {
+      console.log("Ligne Horizontale");
+      gameStatus--;
+    } else if (
+      (game.n1 === game.n4 && game.n4 === game.n7 && game.n1 !== "") ||
+      (game.n2 === game.n5 && game.n5 === game.n8 && game.n2 !== "") ||
+      (game.n3 === game.n6 && game.n6 === game.n9 && game.n1 !== "")
+    ) {
+      console.log("Ligne Verticale");
+      gameStatus--;
+    } else if (
+      (game.n1 === game.n5 && game.n5 === game.n9 && game.n1 !== "") ||
+      (game.n7 === game.n5 && game.n5 === game.n3 && game.n1 !== "") 
+    ) {
+      console.log("Ligne Diagonale");
+      gameStatus--;
     }
-  }
+
+    if (gameStatus === -1) {
+      console.log("Le joueur " + gameInProgress.playerActive + " a gagné!");
+    } else {
+      console.log("La partie continue !");
+    }
+  };
+
+  const changePlayer = () => {
+    if (gameInProgress.playerActive === "X") {
+      setGameInProgress((prevState) => ({
+        ...prevState,
+        playerActive: "O",
+      }));
+    } else {
+      setGameInProgress((prevState) => ({
+        ...prevState,
+        playerActive: "X",
+      }));
+    }
+  };
+  winOrNot(gameInProgress.game, gameInProgress.gameStatus);
+
   return (
     <div className="game">
-      <Board player={playerActive} change={changePlayer} />
+      <Board
+        gameInProgress={gameInProgress}
+        setGameInProgress={setGameInProgress}
+        winOrNot={winOrNot}
+        changePlayer={changePlayer}
+      />
     </div>
   );
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,26 +1,49 @@
 import React, {useState} from "react";
 
 export default function Square(props){
+    // Style
     const styled = { border:'2px solid #000', width: '31%', minHeight: '100px', fontSize: '72px' };
-    const value = props.value;
-    const player = props.player;
+    // Déclaration
+    const nameDiv = props.name;
+
+
+
+    const gameInProgress = props.gameInProgress;
+    const setGameInProgress = props.setGameInProgress
+    const playerActive = gameInProgress.playerActive
+    const winOrNot = props.winOrNot
+    
     const symboleX = "X";
     const symboleO = "O";
     const [symboleUse, setSymboleUse] = useState("")
-    
+
     function handleClick (e){
-        console.log({value})
-        if(player == "X"){
+        console.log({nameDiv})
+        if(playerActive === "X"){
             console.log("X")
             setSymboleUse(symboleX)
-            props.change();
-        } else if (player == "O"){
+            props.changePlayer();
+            setGameInProgress((prevState => ({
+                ...prevState,
+                game: {
+                    ...prevState.game,
+                    [nameDiv]: "X"
+                }})))
+            winOrNot(gameInProgress.game, gameInProgress.gameStatus)
+        } else if (playerActive === "O"){
             console.log("O")
             setSymboleUse(symboleO)
-            props.change();
+            props.changePlayer();
+            setGameInProgress((prevState => ({
+                ...prevState,
+                game: {
+                    ...prevState.game,
+                    [nameDiv]: "O"
+                }})))
+                winOrNot(gameInProgress.game, gameInProgress.gameStatus)
         }
     }    
     return (
-        <div value={value} style={styled} onClick={(e)=> handleClick()}>{symboleUse}</div>
+        <div name={nameDiv} style={styled} onClick={(e)=> handleClick()}>{symboleUse}</div>
     )
 }
